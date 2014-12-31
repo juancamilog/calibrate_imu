937a5b02570b8988bd660a1cdf82b2f2df5e3c1f
Administrator <administrator@kf200-0111.kf200-0111>
2014-12-31 18:19:33 -0500

    Added a launch file

--- build/devel/setup.bash
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# generated from catkin/cmake/templates/setup.bash.in
-
-CATKIN_SHELL=bash
-. "/home/juancamilo/ros_workspace/calibrate_imu/build/devel/setup.sh"
--- nodes/calibrate_imu.py
+++ nodes/calibrate_imu.py
@@ -8,29 +8,44 @@ import math
 import pickle
 from time import localtime,strftime
 import os
-from numpy import linalg,array,amax,amin,add,divide,multiply,subtract,power,Inf,matrix,tile,bmat,random,arange,ones,zeros,eye
+from numpy import linalg,array,amax,amin,add,divide,multiply,subtract,power,Inf,matrix,tile,bmat,random,arange,ones,zeros,eye,squeeze
 
 
 class calibrate_imu:
     def __init__(self):
-        self.__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
         self.mag_samples = []
         self.imu_samples = []
         self.mag_matrix = eye(3)
         self.mag_offset = zeros(3)
         self.sampling = False
+
         mag_topic = rospy.get_param('~mag_topic', '/imu/mag')
         imu_topic = rospy.get_param('~imu_topic', '/imu/data')
+        self.publish_calibrated = rospy.get_param("~publish_calibrated", False)
+        self.max_samples = rospy.get_param("~max_samples", 5000)
+        self.__location__ = rospy.get_param("~calibrations_dir", "/data/mag_calibration")
+        self.calibrated_mag_pub = rospy.Publisher('/imu/mag_calibrated',Vector3Stamped, queue_size='1')
+        self.load_calibration()
+
         rospy.Subscriber(mag_topic,Vector3Stamped,self.mag_cb)
         rospy.Subscriber(imu_topic,Imu, self.imu_cb)
         rospy.Service('start_sampling',Empty,self.start_sampling)
+        rospy.Service('stop_sampling',Empty,self.stop_sampling)
         rospy.Service('calibrate_mag',Empty,self.calibrate_mag)
 
+    def load_calibration(self):
+        file_path = os.path.join(self.__location__,"last_mag_calibration")
+        try:
+            f = open(file_path)
+            self.mag_offset = squeeze(array(pickle.load(f)))
+            self.mag_matrix = pickle.load(f)
+            self.mag_samples = pickle.load(f)
+            f.close()
+        except:
+            rospy.log_error("Could not open file %s"%(file_path))
 
-        self.publish_calibrated = rospy.get_param("publish_calibrated", False)
-        self.calibrated_mag_pub = rospy.Publisher('/imu/mag_calibrated',Vector3Stamped, queue_size='1')
-        rospy.spin()
-        pass
+        rospy.loginfo("Magnetometer offset:\n %s",self.mag_offset)
+        rospy.loginfo("Magnetometer Calibration Matrix:\n %s",self.mag_matrix)
 
     # callback for magnetometer readings
     def mag_cb(self,data):
@@ -39,12 +54,14 @@ class calibrate_imu:
              self.mag_samples.append([data.vector.x,data.vector.y,data.vector.z])
              if len(self.mag_samples)%50==0:
                  rospy.loginfo("Got %d magnetometer readings"%(len(self.mag_samples)))
+             if len(self.mag_samples) > self.max_samples:
+                 # drop the 50 oldest samples
+                 self.mag_samples = self.mag_samples[50:]
 
         if self.publish_calibrated == True:
             mag_raw = array([data.vector.x,data.vector.y,data.vector.z])
-            mag_cal = ((self.mag_matrix.dot((self.mag_raw-self.mag_offset.T).T)).T)
+            mag_cal = ((self.mag_matrix.dot((mag_raw-self.mag_offset.T).T)).T)
             calibrated_mag_msg = Vector3Stamped()
-
             calibrated_mag_msg.header.stamp = data.header.stamp
             calibrated_mag_msg.vector.x = mag_cal[0]
             calibrated_mag_msg.vector.y = mag_cal[1]
@@ -62,8 +79,14 @@ class calibrate_imu:
         self.imu_samples = []
         return EmptyResponse()
 
+    def stop_sampling(self,req):
+        rospy.loginfo('Stopped collecting magnetometer readings')
+        self.sampling = False
+        return EmptyResponse()
+
     def calibrate_mag(self,req):
         if len(self.mag_samples)>=10:
+            self.sampling = False
             xyz = matrix(self.mag_samples)
             rospy.loginfo('Starting magnetometer calibration with %d samples'%(len(self.mag_samples)))
 
@@ -94,7 +117,7 @@ class calibrate_imu:
                         [0.5*q[4][0],0.5*q[5][0],q[2][0]]])
 
             # obtain the centroid of the ellipsoid
-            x0 = (linalg.inv(-1.0*Q)*matrix([0.5*q[6][0],0.5*q[7][0],0.5*q[8][0]]).T)
+            x0 = linalg.inv(-1.0*Q)*matrix([0.5*q[6][0],0.5*q[7][0],0.5*q[8][0]]).T
 
             # translate the ellipsoid in homogeneous coordinates to the center
             T_x0 = matrix(eye(4))
@@ -116,27 +139,29 @@ class calibrate_imu:
             rospy.loginfo("Magnetometer offset:\n %s",x0)
             rospy.loginfo("Magnetometer Calibration Matrix:\n %s",L)
 
-
-            f = open(os.path.join(self.__location__,"last_mag_calibration"),"w+")
+            file_path = os.path.join(self.__location__,"last_mag_calibration")
+            f = open(file_path,"w+")
             pickle.dump(x0,f)
             pickle.dump(L,f)
             pickle.dump(matrix(self.mag_samples),f)
             f.close()
             # back up calibration
-            calib_name = strftime("old_calibrations/mag_calibration_%d_%m_%y_%H_%M_%S",localtime())
+            calib_name = strftime("mag_calibration_%d_%m_%y_%H_%M_%S",localtime())
             calib_path = os.path.join(self.__location__,calib_name)
-            os.system("cp last_mag_calibration %s"%(calib_path))
+            os.system("cp %s %s"%(file_path,calib_path))
 
             self.mag_matrix = L
-            self.mag_offset = x0
+            self.mag_offset = squeeze(array(x0))
         else:
             rospy.loginfo("Not enough samples to calibrate the magnetometer")
         return EmptyResponse()
 
+   
 
 if __name__=='__main__':
     rospy.init_node('calibrate_imu')
     cbimu = calibrate_imu()
+    rospy.spin()
 
 
 
